d6aba9c19dea5f4370f23c6fb9ba41ecdda3f1b4
Vitalii <pecheritsa.vv@gmail.com>
2025-05-23 15:31:20 +0300

    BAC-4842: add google auth

---
+++ src/configs/googleAuth.ts
@@ -0,0 +1,5 @@
+export const googleClientIds = {
+  development: "389651258684-g2eg3hcp17r2ke2n9lpgb9pj59qqplk0.apps.googleusercontent.com",
+  staging: "389651258684-u1bku9lc23lq815j53etruefdeojusfe.apps.googleusercontent.com",
+  production: "389651258684-qv3sruqrahn3v5cea56lb5lgj7pa3rla.apps.googleusercontent.com"
+}
--- src/pages/auth/login/index.vue
+++ src/pages/auth/login/index.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
 import { transformFormData } from 'cardona-core-service/src/helpers'
-import type { ILoginData, LoginForm } from 'cardona-core-service/src/@model/auth'
-import { PageType } from 'cardona-core-service/src/@model/templates/baseSection'
+import { BaseSectionSlots, PageType } from 'cardona-core-service/src/@model/templates/baseSection'
 import BaseSection from 'cardona-core-service/src/components/templates/BaseSection/index.vue'
-import { VColors, VSizes } from 'cardona-core-service/src/@model/vuetify'
+import { VColors, VSizes, VVariants } from 'cardona-core-service/src/@model/vuetify'
+import type { ILoginData, LoginForm } from 'cardona-core-service/src/@model/auth'
 import { useStore } from 'vuex'
+import AppLogo from 'cardona-core-service/src/components/AppLogo.vue'
+import { googleSdkLoaded } from 'vue3-google-login'
 import type { IAuthTokens } from 'axios-jwt'
-import { setAuthTokens } from 'axios-jwt'
-
-import ApiService from 'cardona-core-service/src/services/api'
 import { useAuthForm } from '@/pages/auth/useAuth'
+import { googleClientIds } from '@/configs/googleAuth'
 import { redirectToProject } from '@/helper'
+import ApiService from 'cardona-core-service/src/services/api'
+import { setAuthTokens } from 'axios-jwt'
 
 const store = useStore()
 const formRef = ref()
@@ -18,6 +20,22 @@ const formRef = ref()
 const isLoading = computed(() => {
   return store.getters.isLoadingEndpoint('auth')
 })
+
+const onGoogleAuth = () => {
+  const locationKey = Object.keys(googleClientIds).find(key => location.origin.includes(key))
+  const client_id = locationKey ? googleClientIds[locationKey] : googleClientIds.production
+
+  googleSdkLoaded(google => {
+    google.accounts.oauth2
+      .initCodeClient({
+        client_id,
+        scope: 'openid email profile',
+        ux_mode: 'redirect',
+        redirect_uri: location.origin,
+      })
+      .requestCode()
+  })
+}
 
 const login = async (authData: ILoginData) => {
   const { data }: { data: IAuthTokens } = await ApiService.request({
@@ -49,11 +67,12 @@ const onSubmit = async (form: LoginForm) => {
     <VRow class="auth-inner mx-auto my-auto">
       <div class="align-self-center v-col-4 mx-auto">
         <VCard class="px-6 py-6 d-flex flex-column">
-          <div class="pb-4">
-            <h3 class="text-primary text-uppercase text-center">
-              Authorization service
-            </h3>
+          <div class="brand-logo mx-auto">
+            <AppLogo />
           </div>
+          <VCardTitle class="mx-auto">
+            {{ $t('page.login.welcomeToAdminPanel') }}
+          </VCardTitle>
           <BaseSection
             ref="formRef"
             :page-type="PageType.Create"
@@ -66,7 +85,7 @@ const onSubmit = async (form: LoginForm) => {
               </div>
             </template>
 
-            <template #actions="{ form }">
+            <template #[BaseSectionSlots.Actions]="{ form }: { form: LoginForm }">
               <VBtn
                 :color="VColors.Primary"
                 :size="VSizes.Large"
@@ -79,12 +98,37 @@ const onSubmit = async (form: LoginForm) => {
                   indeterminate
                 />
                 <template v-else>
-                  {{ $t('action.login') }}
+                  <span class="login-btn-text">
+                    {{ $t('action.login') }}
+                  </span>
                 </template>
               </VBtn>
             </template>
           </BaseSection>
-          <VCardText class="text-center mt-2 pb-0">
+
+          <div class="or-separator my-6">
+            {{ $t('page.login.or') }}
+          </div>
+
+          <VBtn
+            :color="VColors.Secondary"
+            :variant="VVariants.Outlined"
+            :size="VSizes.Large"
+            class="full-width"
+            @click="onGoogleAuth"
+          >
+            <img
+              class="google-logo mr-2"
+              alt="google-logo"
+              src="../../../assets/images/google-logo.png"
+            >
+
+            <span class="text-button">
+              {{ $t('page.login.signInWithGoogle') }}
+            </span>
+          </VBtn>
+
+          <VCardText class="text-center pb-0">
             <span>{{ $t('page.login.inCaseForgotPassword') }}</span>
           </VCardText>
         </VCard>
@@ -97,5 +141,37 @@ const onSubmit = async (form: LoginForm) => {
 .auth-wrapper {
   height: 100dvh;
   display: flex;
+
+  .login-btn-text {
+    font-size: 0.9375rem;
+  }
+
+  .or-separator {
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+
+    &:before,
+    &:after {
+      content: '';
+      display: inline-block;
+      vertical-align: middle;
+      box-sizing: border-box;
+      height: 1px;
+      flex: 1 1 auto;
+      background: rgba(var(--v-border-color),var(--v-border-opacity));
+    }
+    &:before {
+      margin-right: 0.5rem;
+    }
+    &:after {
+      margin-left: 0.5rem;
+    }
+  }
+
+  .google-logo {
+    height: 20px;
+    width: 20px;
+  }
 }
 </style>
